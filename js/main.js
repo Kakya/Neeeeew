75c42c943916c9420963dce8c08bfdfa33121150
Kakya <husseinzamzamy14@gmail.com>
2015-04-09 09:14:09 -0400

    More debugging

--- js/main.js
+++ js/main.js
@@ -196,24 +196,24 @@ function update()
 	fDice = game.rnd.integerInRange(0,18)
 	 if (cursors.up.isDown)
     {
-        game.physics.arcade.accelerationFromRotation(sprite.rotation, 200, sprite.body.acceleration);
+        game.physics.arcade.accelerationFromRotation(card.rotation, 200, card.body.acceleration);
     }
     else
     {
-        sprite.body.acceleration.set(0);
+        card.body.acceleration.set(0);
     }
 
     if (cursors.left.isDown)
     {
-        sprite.body.angularVelocity = -300;
+        card.body.angularVelocity = -300;
     }
     else if (cursors.right.isDown)
     {
-        sprite.body.angularVelocity = 300;
+        card.body.angularVelocity = 300;
     }
     else
     {
-        sprite.body.angularVelocity = 0;
+        card.body.angularVelocity = 0;
     }
 	if(fDice <6)
 	{
